a1ad2c5f91469844a38d3e5ebe8c0a0491c1ceb9
Scoophead <vincentt.montes@gmail.com>
2024-12-02 12:44:55 -0500

    first assessment

--- ts-1-variables/day1.js
+++ ts-1-variables/day1.js
@@ -1,9 +1,22 @@
-console.log("JavaScript!!!");
-// alert("UH OHH")
-var balance = 100;
-console.log(typeof balance);
-var mutableBalance = 1000;
-mutableBalance = 20000;
-var deposit = Number(prompt('deposit amount'));
-mutableBalance += deposit;
-console.log('new balance is', mutableBalance);
+{
+    //     console.log("JavaScript!!!")
+    //     const balance:number = 100
+    //     console.log(typeof balance)
+    //     let mutableBalance:number = 1000
+    //     mutableBalance = 20000
+    //     console.log(mutableBalance)
+    //     let deposit: number = Number(prompt('deposit amount'))
+    //     mutableBalance += deposit
+    //     console.log('new balance is', mutableBalance)
+    //-----------------------------------------------
+    var myNumber = 1006;
+    var myString = 'testing';
+    var myBool = true;
+    console.log(typeof myNumber, myNumber);
+    console.log(typeof myString, myString);
+    console.log(typeof myBool, myBool);
+    //----------------------------------------------
+    var greeting = 'hello';
+    var name_1 = prompt('what is your name?');
+    console.log(greeting, name_1);
+}
--- ts-1-variables/day1.ts
+++ ts-1-variables/day1.ts
@@ -1,18 +1,36 @@
 {
-    console.log("JavaScript!!!")
-    // alert("UH OHH")
+//     console.log("JavaScript!!!")
     
-    const balance:number = 100
-    console.log(typeof balance)
+//     const balance:number = 100
+//     console.log(typeof balance)
     
-    let mutableBalance:number = 1000
-    mutableBalance = 20000
+//     let mutableBalance:number = 1000
+//     mutableBalance = 20000
 
-    console.log(mutableBalance)
+//     console.log(mutableBalance)
     
-    let deposit: number = Number(prompt('deposit amount'))
+//     let deposit: number = Number(prompt('deposit amount'))
     
-    mutableBalance += deposit
+//     mutableBalance += deposit
     
-    console.log('new balance is', mutableBalance)
+//     console.log('new balance is', mutableBalance)
+
+    //-----------------------------------------------
+
+    let myNumber:number = 1006
+    let myString:String = 'testing'
+    let myBool:boolean = true
+
+    console.log(typeof myNumber, myNumber)
+    console.log(typeof myString, myString)
+    console.log(typeof myBool, myBool)
+
+    //----------------------------------------------
+
+    let greeting:String = 'hello'
+
+    let name:String | null = prompt('what is your name?')
+    
+    console.log(greeting, name)
+    
 }
